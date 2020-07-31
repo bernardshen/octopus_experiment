e78c27f7734cc44d7a53aec18d7d8fd943568abf
bernard <jcshen1230@126.com>
2020-07-31 11:22:47 +0800

    grh config

--- src/net/RdmaSocket.cpp
+++ src/net/RdmaSocket.cpp
@@ -298,15 +298,19 @@ bool RdmaSocket::ModifyQPtoRTR(struct ibv_qp *qp, uint32_t remote_qpn, uint16_t 
     attr.ah_attr.sl = 0;
     attr.ah_attr.src_path_bits = 0;
     attr.ah_attr.port_num = Port;
-    // if (GidIndex >= 0) {
-    //     attr.ah_attr.is_global = 1;
-    //     attr.ah_attr.port_num = 1;
-    //     memcpy(&attr.ah_attr.grh.dgid, dgid, 16);
-    //     attr.ah_attr.grh.flow_label = 0;
-    //     attr.ah_attr.grh.hop_limit = 1;
-    //     attr.ah_attr.grh.sgid_index = GidIndex;
-    //     attr.ah_attr.grh.traffic_class = 0;
-    // }
+
+    // sxxxx
+    if (GidIndex >= 0) {
+        attr.ah_attr.is_global = 1;
+        attr.ah_attr.port_num = 1;
+        memcpy(&attr.ah_attr.grh.dgid, dgid, 16);
+        attr.ah_attr.grh.flow_label = 0;
+        attr.ah_attr.grh.hop_limit = 1;
+        attr.ah_attr.grh.sgid_index = GidIndex;
+        attr.ah_attr.grh.traffic_class = 0;
+    }
+    // exxxx
+    
     flags = IBV_QP_STATE | IBV_QP_AV | IBV_QP_PATH_MTU | IBV_QP_DEST_QPN | IBV_QP_RQ_PSN;
     // IBV_QP_MAX_DEST_RD_ATOMIC | IBV_QP_MIN_RNR_TIMER
     if (Mode == 0) {
